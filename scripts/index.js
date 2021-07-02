404eaa1acd9a32bc35462ba3ac57c36baaa57803
Alexei Volny <pancfly@yandex.ru>
2021-07-02 22:58:22 +0300

    project 5

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,126 @@
 const content = document.querySelector('.content');
 const profile = content.querySelector('.profile');
 const popupEditOpen = profile.querySelector('.profile__button_action_edit');
-const popupElement = document.querySelector('.popup');
-const popupEditClose = popupElement.querySelector('.popup__button_close');
-const popupForm = popupElement.querySelector('.popup__input');
+const popupElementEdit = document.querySelector('.popup-edit');
+const popupEditClose = popupElementEdit.querySelector('.popup__button_close-edit');
+const popupFormEdit = popupElementEdit.querySelector('.popup__input');
 let profileName = profile.querySelector('.profile__name');
 let profileAbout = profile.querySelector('.profile__about');
-let inputName = popupForm.querySelector('.popup__text_type_name');
-let inputAbout = popupForm.querySelector('.popup__text_type_about');
+let inputName = popupFormEdit.querySelector('.popup__text_type_name');
+let inputAbout = popupFormEdit.querySelector('.popup__text_type_about');
 
-/*открытие*/
-function openPopup() {
-    popupElement.classList.add('popup_is-opened');
-    inputName.value = profileName.textContent;
-    inputAbout.value = profileAbout.textContent;
+/*elements*/
+const popupElements = document.querySelector('.popup-add');
+const cardData = document.querySelector('.popup__elements-form');
+const popupOpenButtonElement = document.querySelector('.profile__button_action_add');
+const popupCloseButtonElement = document.querySelector('.popup__button_close-add');
+const elements = document.querySelector('.elements__list');
+const templateElements = document.querySelector('.template-elements');
+const cardTitle = cardData.querySelector('.popup__card-name');
+const cardImageSrc = cardData.querySelector('.popup__card-src');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+/*переменные fullscreen попапа*/
+const popupFullscreen = document.querySelector('.popup_type_fullscreen');
+const fullscreenImage = document.querySelector('.popup__fullscreen-image');
+const fullscreenText = document.querySelector('.popup__fullscreen-text');
+const fullscreenCloseButton = document.querySelector('.popup__button_close-fullscreen');
+
+/*ф-я слушатель*/
+function addEventListeners (element) {
+    element.querySelector('.elements__button_delete').addEventListener('click', deleteCard);
+    element.querySelector('.elements__button_like').addEventListener('click', likeCard);
+    element.querySelector('.elements__image').addEventListener('click', openFullscreen);
 }
 
-/*закрытие*/
-function closePopup() {
-    popupElement.classList.remove('popup_is-opened');
+/*ф-и elements*/
+function createCard(cardData) {
+    const element = templateElements.content.cloneNode(true);
+    const elementTitle = element.querySelector('.elements__title');
+    const elementImage = element.querySelector('.elements__image');
+    elementTitle.textContent = cardData.name;
+    elementImage.src = cardData.link;
+    elementImage.alt = cardData.name;
+    addEventListeners(element);
+    return element;
+}
+  
+function cardPlacement (element) {
+    elements.prepend(element);
+}
+  
+initialCards.forEach(cardData => {
+    const element = createCard(cardData);
+    cardPlacement(element);
+})
+  
+function deleteCard (evt) {
+    const element = evt.target.closest('.elements__element')
+    element.remove();
+}
+  
+function likeCard (evt) {
+    evt.target.classList.toggle('elements__button_like-active');
+}
+  
+function handleOpenAddCardPopup () {
+    popupToggle(popupElements);
+    cardTitle.value = '';
+    cardImageSrc.value = '';
+}
+  
+function elementsInfoEdit (event) {
+    event.preventDefault();
+    const element = createCard({
+      name: cardTitle.value,
+      link: cardImageSrc.value,
+    });
+    popupToggle(popupElements);
+    cardPlacement(element);
+}
+
+/* ф-и fullscreen*/
+function openFullscreen (evt) {
+    const element = evt.target.closest('.elements__image');
+    fullscreenImage.src = element.src;
+    fullscreenText.textContent = element.alt;
+    popupToggle(popupFullscreen);
+}
+
+const popupToggle = function (popup) {
+    popup.classList.toggle('popup_is-opened');
+}
+
+/*открытие*/
+function handleOpenProfilePopup() {
+    popupToggle(popupElementEdit);
+    inputName.value = profileName.textContent;
+    inputAbout.value = profileAbout.textContent;
 }
 
 /*редактирование с сохранением*/
@@ -27,10 +129,15 @@ function editNameAbout (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileAbout.textContent = inputAbout.value;
-    closePopup();
+    popupToggle(popupElementEdit);
 }
 
 /*вызов*/
-popupEditOpen.addEventListener('click', openPopup);
-popupEditClose.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', editNameAbout);
+popupEditOpen.addEventListener('click', handleOpenProfilePopup);
+popupEditClose.addEventListener('click', () => popupToggle(popupElementEdit));
+popupFormEdit.addEventListener('submit', editNameAbout);
+
+popupOpenButtonElement.addEventListener('click', handleOpenAddCardPopup);
+popupCloseButtonElement.addEventListener('click', () => popupToggle(popupElements));
+fullscreenCloseButton.addEventListener('click', () => popupToggle(popupFullscreen));
+cardData.addEventListener('submit', elementsInfoEdit);
