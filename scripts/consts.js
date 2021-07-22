f686f01f3855cc5545c277b275d71b68a6881f56
Pancfly <83037203+Pancfly@users.noreply.github.com>
2021-07-22 11:18:43 +0300

    Revert "add opacity popup"

---
+++ scripts/consts.js
@@ -0,0 +1,31 @@
+export const validationSettings = {
+    inputSelector: '.popup__text',
+    submitButtonSelector: '.popup__button_submit',
+    inputErrorClass: 'popup__text_type_error',
+    errorClass: 'popup__text-error_active'
+}
+
+export const content = document.querySelector('.content');
+export const photoGallery = document.querySelector('.elements__list');
+export const forms = Array.from(document.querySelectorAll('.popup__input'));
+export const popupElementEdit = document.querySelector('.popup-edit');
+export const popupFormEdit = popupElementEdit.querySelector('.popup__profile-form');
+export const popupEditClose = popupElementEdit.querySelector('.popup__button_close-edit');
+export const popupEditSubmit = popupElementEdit.querySelector('.popup__button_submit');
+export const inputName = popupFormEdit.querySelector('#profile-name');
+export const inputAbout = popupFormEdit.querySelector('#profile-about');
+export const popupEditOpen = content.querySelector('.profile__button_action_edit');
+export const profileName = content.querySelector('.profile__name');
+export const profileAbout = content.querySelector('.profile__about');
+export const popupElements = document.querySelector('.popup-add');
+export const popupFormAdd = popupElements.querySelector('.popup__elements-form');
+export const popupAddClose = popupElements.querySelector('.popup__button_close-add');
+export const buttonAddSubmit = popupElements.querySelector('.popup__button_submit');
+export const cardName = document.querySelector('#card-name');
+export const cardLink = document.querySelector('#card-link');
+export const popupAddOpen = content.querySelector('.profile__button_action_add');
+export const templateElements = document.querySelector('.template-elements');
+export const popupFullscreen = document.querySelector('.popup_type_fullscreen');
+export const popupFullscreenClose = popupFullscreen.querySelector('.popup__button_close-fullscreen');
+export const fullscreenImage = popupFullscreen.querySelector('.popup__fullscreen-image');
+export const fullscreenText = popupFullscreen.querySelector('.popup__fullscreen-text');
